b728ec58c8d960297c57282234a7e1898e2fcac3
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-11-06 12:55:10 +0200

    #6 Navigation between screens

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -2,8 +2,12 @@ import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'edit_blogpost':
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     case 'delete_blogpost':
-      return state.filter(blogPost => blogPost.id !== action.payload);
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     case 'add_blogpost':
       return [
         ...state,
@@ -11,27 +15,40 @@ const blogReducer = (state, action) => {
           id: Math.floor(Math.random() * 99999),
           title: action.payload.title,
           content: action.payload.content,
-        }
+        },
       ];
     default:
       return state;
   }
 };
 
-const addBlogPost = dispatch => {
+const addBlogPost = (dispatch) => {
   return (title, content, callback) => {
     dispatch({ type: 'add_blogpost', payload: { title, content } });
-    callback();
+    if (callback) {
+      callback();
+    }
   };
 };
-const deleteBlogPost = dispatch => {
-  return id => {
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
     dispatch({ type: 'delete_blogpost', payload: id });
+  };
+};
+const editBlogPost = (dispatch) => {
+  return (id, title, content, callback) => {
+    dispatch({
+      type: 'edit_blogpost',
+      payload: { id, title, content },
+    });
+    if (callback) {
+      callback();
+    }
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
-  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1}]
+  { addBlogPost, deleteBlogPost, editBlogPost },
+  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 );
