19053ac7dec218c5d4403c5bb1f796c6ef48bdd2
Bogdan Mihalache <bogdan.mihalache@calosense.com>
2023-11-06 12:39:49 +0200

    #6 Create and Edit Screen

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,13 +1,14 @@
-import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext,  } from 'react';
+import {  StyleSheet } from 'react-native';
 import { Context } from '../context/BlogContext';
+import BlogPostForm from '../components/BlogPostForm';
 
 const CreateScreen = ({ navigation }) => {
-  return (
-    <View>
-      <Text>Create</Text>
-    </View>
-  );
+    const { addBlogPost } = useContext(Context);
+
+    return <BlogPostForm onSubmit={(title, content) => {
+        addBlogPost(title, content, () => navigation.navigate('Index'))
+    }} />
 };
 
 const styles = StyleSheet.create({});
